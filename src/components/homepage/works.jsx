39586d60860634ee00ba6abe1482df500e046345
sompal <sompal7678@gmail.com>
2025-04-04 13:59:18 +0530

    fix: experience

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -15,18 +15,18 @@ const Works = () => {
 					<div className="works-body">
 						<div className="work">
 							<img
-								src="./facebook.png"
-								alt="facebook"
+								src="https://media.licdn.com/dms/image/v2/C510BAQGnSHf-htBtxA/company-logo_200_200/company-logo_200_200/0/1630610714444/qwerty_thoughts_logo?e=1749081600&v=beta&t=062dPqP_Vh9zLY08NfQZBduv8UecZaPaTttibs5BX4M"
+								alt="qwerty_thoughts"
 								className="work-image"
 							/>
-							<div className="work-title">Facebook</div>
+							<div className="work-title">Qwerty Thoughts</div>
 							<div className="work-subtitle">
-								Software Engineer
+								Associate React.js Developer
 							</div>
-							<div className="work-duration">2019 - Present</div>
+							<div className="work-duration">2022 - 2025</div>
 						</div>
 
-						<div className="work">
+						{/* <div className="work">
 							<img
 								src="./twitter.png"
 								alt="twitter"
@@ -37,7 +37,7 @@ const Works = () => {
 								Software Engineer
 							</div>
 							<div className="work-duration">2019 - Present</div>
-						</div>
+						</div> */}
 					</div>
 				}
 			/>
